f3b23424bc43d243854a37de64ed214ec931218e
huihui <503044120@qq.com>
2016-06-12 21:56:36 +0800

    测试提交

--- app/src/main/java/com/huihui/androidstudy/RegisterActivity.java
+++ app/src/main/java/com/huihui/androidstudy/RegisterActivity.java
@@ -1,7 +1,9 @@
 package com.huihui.androidstudy;
 
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by huihui on 2016/6/10.
  */
-public class RegisterActivity  {
+public class RegisterActivity extends AppCompatActivity {
 }
